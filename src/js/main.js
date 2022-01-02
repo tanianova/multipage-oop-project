a91f05364528cbc94275af32775f9b19cc1d193a
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-02 13:27:44 +0300

    added sliders

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import Slider from "./modules/slider";
+
+window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    const slider = new Slider('.page','.next');
+    slider.render();
+});
