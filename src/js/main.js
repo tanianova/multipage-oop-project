fb8a1cd2b6eda3ba3584338500fd08b266808262
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-02 15:01:55 +0300

    added modal with video from youtube API

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import Slider from "./modules/slider";
+import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     const slider = new Slider('.page','.next');
     slider.render();
+    const player = new VideoPlayer('.showup .play','.overlay');
+    player.init();
 });
