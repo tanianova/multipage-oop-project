704817ea7049195fd29953bf70b62d8266038a8f
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-03 10:49:20 +0300

    added accordion; downloading files from project

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import Difference from "./modules/difference";
 import Form from "./modules/forms";
+import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -53,4 +55,8 @@ window.addEventListener('DOMContentLoaded', () => {
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
 
     new Form('.form').init();
+
+    new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 });
---
+++ src/js/modules/download.js
@@ -0,0 +1,25 @@
+export default class Download {
+    constructor(triggers) {
+        this.btns = document.querySelectorAll(triggers);
+        this.path = 'assets/img/mainbg.jpg';
+    }
+
+    downloadItem(path) {
+        const elem = document.createElement('a');
+        elem.setAttribute('href', this.path);
+        elem.setAttribute('download', 'nice_picture');
+        elem.style.display = 'none';
+        document.body.appendChild(elem);
+
+        elem.click();
+        document.body.removeChild(elem);
+    }
+
+    init() {
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                this.downloadItem(this.path);
+            });
+        });
+    }
+}
